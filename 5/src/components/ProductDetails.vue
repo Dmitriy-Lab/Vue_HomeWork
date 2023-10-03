be49a3db64df398b8d69dcc7fd766d22b35dda9f
Dmitry <poznavshii@gmail.com>
2023-10-03 15:40:20 +0400

    add 5 homeWork

---
+++ 5/src/components/ProductDetails.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <p>Название: {{ product.name }}, цена: {{ formattedPrice }}, доступен:{{ isProdAvailable }}</p>
+        <button @click="changeAvailable">{{ isProdAvailable }}?</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductDetails',
+
+    data() {
+        return {
+            product: {
+                name:'IPhone',
+                price: 1000,
+                available: true,
+            },
+        };
+    },
+
+    computed: {
+        formattedPrice() {
+            return `$${this.product.price}`;
+        },
+        isProdAvailable() {
+            if (this.product.available) {
+                return 'available'
+            } else {
+                return 'Out of stock'
+            }
+        }
+    },
+
+    methods: {
+        changeAvailable() {
+            console.log(this.product.available);
+            this.product.available = !this.product.available;
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
